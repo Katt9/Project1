938b9aaf97ccd50374976529238efb28b62151b7
Kylie <kyliehumble@yahoo.com>
2019-10-19 10:05:17 -0500

    modified orders/CustomerOrder.py

--- orders/CustomerOrder.py
+++ orders/CustomerOrder.py
@@ -1,16 +1,24 @@
 class CustomerOrder:
     def __init__(self, order_id, customer_id):
         # initialize self.order_id, self.customer_id, and self.attributes_list
-        pass
+        self.order_id = order_id
+        self.customer_id = customer_id
+        self.attributes_list = []
     
     def append(self, attributes):
-        pass
+        self.attributes_list.append(attributes)
     
     def calculate_order_total(self):
-        pass
+        subtotal = 0
+        for attribute in self.attributes_list:
+            subtotal += attribute.quantity * attribute.price
+        return subtotal
     
     
-    class CustomerOrderAttributes:
-        def __init__(self, product_id, quantity, price, region):
-            # initialize self.product_id, self.quantity, self.price, self.region
-            pass
+class CustomerOrderAttributes:
+    def __init__(self, product_id, quantity, price, region):
+        # initialize self.product_id, self.quantity, self.price, self.region
+        self.product_id = product_id
+        self.quantity = quantity
+        self.price = price
+        self.region = region
