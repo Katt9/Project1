197bb0ed4c6ce7c0ac91245330cf48a397967583
Kylie <kyliehumble@yahoo.com>
2019-11-09 10:58:17 -0600

    commit changes

--- tests/orders/customer_order_test.py
+++ tests/orders/customer_order_test.py
@@ -2,8 +2,35 @@ from orders.CustomerOrder import CustomerOrder
 import pytest
 
 
+class MockCustomerOrderAttributes:
+    def __init__(self, quantity, price):
+        self.quantity = quantity
+        self.price = price
+
+
 def test_init():
     new_order = CustomerOrder("test_order_id", "test_customer_id")
     assert new_order.order_id == "test_order_id"
     assert new_order.customer_id == "test_customer_id"
     assert len(new_order.attributes_list) == 0
+
+
+def test_append():
+    new_order = CustomerOrder("test_order_id", "test_customer_id")
+    new_order.append("test attributes")
+
+    assert len(new_order.attributes_list) == 1
+    assert new_order.attributes_list[0] == "test attributes"
+
+
+def test_calculate_order_total():
+    new_order = CustomerOrder("test_order_id", "test_customer_id")
+    new_customer_attr1 = MockCustomerOrderAttributes(1, 5)
+    new_customer_attr2 = MockCustomerOrderAttributes(2, 10)
+    new_customer_attr3 = MockCustomerOrderAttributes(3, 15)
+    expected_total = 70
+    new_order.append(new_customer_attr1)
+    new_order.append(new_customer_attr2)
+    new_order.append(new_customer_attr3)
+    total = new_order.calculate_order_total()
+    assert total == expected_total
